086e4aa33ca60a62272b046f5084e620c7c95341
Orlando Álvarez <wiki97ap@gmail.com>
2021-04-05 14:49:41 -0500

    Functional Requirements
    Functional requirements completed.

--- inmobiliaria/components/listItem.js
+++ inmobiliaria/components/listItem.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
-
+import React, { useState } from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity, Button } from 'react-native'
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
+import {faBath, faBed, faHeart, faMapMarkerAlt, faRuler} from '@fortawesome/free-solid-svg-icons'
 const ListItem = ({ name, adress, rooms, bathrooms, area, rent, imageUrl, isFav, rate }) => {
+
+    const [fav, setFav]= useState(isFav)
+
     return (
         <View style={styles.itemContainer}>
             <View style={styles.leftItemsContainer}>
                 <Image style={styles.itemImage} source={imageUrl} />
                 <View style={styles.itemRate}>
-                    <Text>{rate} *</Text>
+                    <Text style={styles.itemRateText}>⭐{rate}</Text>
                 </View>
             </View>
             <View style={styles.rightItemsContainer}>
-                <Text style={styles.itemTitle}>{name}</Text>
-                <Text>{adress}</Text>
+                <Text style={styles.itemTitle} >{name}</Text>
+                <Text><FontAwesomeIcon icon={faMapMarkerAlt}  style={{color:'grey'}} /> {adress}</Text>
                 <View style={styles.itemServices}>
-                    <Text>{rooms}</Text>
-                    <Text>{bathrooms}</Text>
-                    <Text>{area} ft. ^2</Text>
+                    <Text style={{fontWeight:'bold'}}><FontAwesomeIcon icon={faBed}  style={{color:'grey'}}  /> {rooms}</Text>
+                    <Text style={{fontWeight:'bold'}}><FontAwesomeIcon icon={faBath} style={{color:'grey'}}/> {bathrooms}</Text>
+                    <Text style={{fontWeight:'bold'}}><FontAwesomeIcon icon={faRuler} style={{color:'grey'}}/> {area} ft ²</Text>
                 </View>
                 <View style={styles.itemDets}>
-                    <Text>${rent}/m</Text>
-                    <Text>{isFav ? 'fav' : 'No fav'}</Text>
+                    <Text style={{fontWeight:'bold'}}>${rent}/m</Text>
+                    <TouchableOpacity onPress={()=>{setFav(!fav)}} >{fav == true ? <FontAwesomeIcon icon={faHeart} style={{color:'green'}} /> : <FontAwesomeIcon icon={faHeart} style={{color:'grey'}}/>}</TouchableOpacity>
                 </View>
             </View>
         </View>
@@ -43,10 +47,12 @@ const styles = StyleSheet.create({
         margin: 10
     },
     itemServices: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent:'space-between'
     },
     itemDets: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent:'space-between'
     },
     itemImage: {
         height: 100,
@@ -55,11 +61,21 @@ const styles = StyleSheet.create({
     },
     itemTitle: {
         fontSize: 15,
+        fontWeight: "bold",
     },
     itemRate: {
-        position: 'absolute',
-
-    }
+        backgroundColor: '#FBEDB7',
+        borderRadius: 10,
+        width: 45,
+        left: 30,
+        bottom: 25
+    },
+    itemRateText: {
+        color: '#7A6229'
+    },
+    boldText: {
+        fontWeight: 'bold'
+    },
 })
 
 export default ListItem
